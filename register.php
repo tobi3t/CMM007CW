1ca8a368746eeca465da172d9603083d111249ce
Emmanuel <tobi3t@gmail.com>
2023-03-08 13:28:28 +0000

    extention renaming

--- register.php
+++ register.php
@@ -1,39 +1,74 @@
-<?php
+<!DOCTYPE html>
+<html>
 
-$name = $email = $password = $confirm_password = "";
-$name_error = $email_error = $password_error = $confirm_password_error = "";
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Register</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
+    <link rel="stylesheet" type="text/css" href="assets/style.css">
+</head>
 
-if(isset($_POST['register'])) {
-	// Validate name
-	if(empty($_POST['name'])) {
-		$name_error = "Name is required";
-	} else {
-		$name = test_input($_POST['name']);
-		if(!preg_match("/^[a-zA-Z ]*$/", $name)) {
-			$name_error = "Only letters and white space allowed";
-		}
-	}
+<body>
+    <header>
+        <nav>
+            <ul id>
+                <li><a href="index.html">Home</a></li>
+                <li><a href="browsestories.php">Stories</a></li>
+                <li><a href="login.html">Login</a></li>
+                <li><a href="register.php">Sign Up</a></li>
+                <li><a href="contactus.html">Contact Us</a></li>
+            </ul>
+        </nav>
+    </header>
+    <main>
+        <h2>Sign Up</h2>
+        <div class="formholder">
+            <form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>">
+                <label for="name">Name:</label>
+                <input type="text" name="name" value="<?php echo isset($_POST["name"]) ? htmlspecialchars($_POST["name"]) : ""; ?>" required>
+                <label for="email">Email:</label>
+                <input type="email" name="email" value="<?php echo isset($_POST["email"]) ? htmlspecialchars($_POST["email"]) : ""; ?>" required>
+                <label for="password">Password:</label>
+                <input type="password" name="password" required>
+                <label for="confirm_password">Confirm Password:</label>
+                <input type="password" name="confirm_password" required>
+                <button type="submit" name="register">Register</button>
+            </form>
+        </div>
+        <h6>Already have an account? <a href="login.html">Login here</a></h6>
+    </main>
+    <footer>
+        <p>&copy; 2023 Touries Inc. All rights reserved.</p>
+        <div class="container">
+            <div class="row">
+                <div class="col">
+                    <nav>
+                        <ul>
+                            <li><a href="faq.html">Frequently Asked Questions</a></li>
+                        </ul>
+                    </nav>
+                </div>
+                <div class="col">
+                    <nav>
+                        <ul>
+                            <li><a href="contactus.html">Reader's Feedback</a></li>  
+                        </ul>
+                    </nav>
+                </div>
+            </div>
+        </div>
+    </footer>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN"
+        crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js"
+        integrity="sha384-oBqDVmMz9ATKxIep9tiCxS/Z9fNfEXiDAYTujMAeBAsjFuCZSmKbSSUnQlmh/jp3"
+        crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.min.js"
+        integrity="sha384-mQ93GR66B00ZXjt0YO5KlohRA5SY2XofN4zfuZxLkoj1gXtW8ANNCe9d5Y3eG5eD"
+        crossorigin="anonymous"></script>
+</body>
 
-	// Validate email
-	if(empty($_POST['email'])) {
-		$email_error = "Email is required";
-	} else {
-		$email = test_input($_POST['email']);
-		if(!filter_var($email, FILTER_VALIDATE_EMAIL)) {
-			$email_error = "Invalid email format";
-		}
-	}
-
-	// Validate password
-	if(empty($_POST['password'])) {
-		$password_error = "Password is required";
-	} else {
-		$password = test_input($_POST['password']);
-		if(strlen($password) < 8) {
-			$password_error = "Password must be at least 8 characters";
-		}
-	}
-
-	// Validate confirm password
-	if(empty($_POST['confirm_password'])) {
-		$confirm_password_error
+</html>s
