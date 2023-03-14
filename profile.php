5e69482b2dd15b4a2f12bdf06daff2704dd962f4
Emmanuel <tobi3t@gmail.com>
2023-03-14 19:36:52 +0000

    Table of Stories

--- profile.php
+++ profile.php
@@ -7,6 +7,27 @@ if (!isset($_SESSION['authenticated']) || $_SESSION['authenticated'] != true) {
   header('Location: index.php');
   exit;
 }
+include("connection.php");
+
+// Get the user ID from the URL parameter
+$user_id = $_SESSION['user_id'];
+
+// Create a new database connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+// Check if the connection was successful
+if ($conn->connect_error) {
+  die('Connection failed: ' . $conn->connect_error);
+}
+
+// Prepare the SQL statement to select stories by user ID
+$sql = "SELECT id, title, location, story, image FROM stories WHERE user_id = ?";
+$stmt = $conn->prepare($sql);
+$stmt->bind_param("i", $user_id);
+
+// Execute the statement
+$stmt->execute();
+$result = $stmt->get_result();
 ?>
 
 <!DOCTYPE html>
@@ -31,13 +52,31 @@ if (!isset($_SESSION['authenticated']) || $_SESSION['authenticated'] != true) {
         </nav>
     </header>
     <main>
-        <div class="container">
-            <div class="row">
-                <div class="col">Col One</div>
-                <div class="col">Col Two</div>
-                <div class="col">Col Three</div>
-            </div>
-        </div>
+    <h1>My Stories</h1>
+  <table>
+    <thead>
+      <tr>
+        <th>Title</th>
+        <th>Location</th>
+        <th>Story</th>
+        <th>Image</th>
+        <th>Edit</th>
+        <th>Delete</th>
+      </tr>
+    </thead>
+    <tbody>
+      <?php while ($row = $result->fetch_assoc()): ?>
+        <tr>
+          <td><?php echo $row['title']; ?></td>
+          <td><?php echo $row['location']; ?></td>
+          <td><?php echo $row['story']; ?></td>
+          <td><?php echo $row['image']; ?></td>
+          <td><a href="editstory.php?id=<?php echo $row['id']; ?>">Edit</a></td>
+          <td><a href="delete_story.php?id=<?php echo $row['id']; ?>">Delete</a></td>
+        </tr>
+      <?php endwhile; ?>
+    </tbody>
+  </table>
     </main>
     <footer>
         <p>&copy; 2023 Touries Inc. All rights reserved.</p>
